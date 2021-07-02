2cc282ec8b6a4674ce5f98e93f420e8e26ff4a35
Venugopal K <venu.shastri@gmail.com>
2021-07-02 17:32:23 +0530

    Update Program.cs

--- Image-GallerySolution/Program.cs
+++ Image-GallerySolution/Program.cs
@@ -11,17 +11,16 @@ namespace Image_GallerySolution
         static void Main(string[] args)
         {
             Whatsapp _whatsapp = new Whatsapp();
-            ImageGallery _imageGallery1 = new ImageGallery(_whatsapp);
-            _imageGallery1.Share();
-
             Gmail _gmail = new Gmail();
-            ImageGallery _imageGallery2 = new ImageGallery(_gmail);
-            _imageGallery2.Share();
-
             Bluetooth _bluetooth = new Bluetooth();
-            ImageGallery _imageGallery3 = new ImageGallery(_bluetooth);
-            _imageGallery3.Share();
-
+            
+            
+            ImageGallery _imageGallery = new ImageGallery();
+            _imageGallery.Share(_whatsapp);//_whatsapp
+            _imageGallery.Share(_gmail);
+            _imageGallery.Share(_bluetooth);
+        
+           
             
         }
 
